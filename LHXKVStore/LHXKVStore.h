e37f4cf9b787415223771d4e8cf0cfaaa4d021bd
freshman-kyle <469328423@qq.com>
2020-01-05 17:01:57 +0800

    Initial Commit

---
+++ LHXKVStore/LHXKVStore.h
@@ -0,0 +1,19 @@
+//
+//  LHXKVStore.h
+//  LHXKVStore
+//
+//  Created by 李华夏 on 2020/1/5.
+//  Copyright © 2020 李华夏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LHXKVStore.
+FOUNDATION_EXPORT double LHXKVStoreVersionNumber;
+
+//! Project version string for LHXKVStore.
+FOUNDATION_EXPORT const unsigned char LHXKVStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LHXKVStore/PublicHeader.h>
+
+
---
+++ LHXKVStoreTests/LHXKVStoreTests.m
@@ -0,0 +1,37 @@
+//
+//  LHXKVStoreTests.m
+//  LHXKVStoreTests
+//
+//  Created by 李华夏 on 2020/1/5.
+//  Copyright © 2020 李华夏. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LHXKVStoreTests : XCTestCase
+
+@end
+
+@implementation LHXKVStoreTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
